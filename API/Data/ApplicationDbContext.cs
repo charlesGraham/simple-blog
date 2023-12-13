31c15c6f8a0826931d7fa0120a7260c4de7381c1
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-13 12:31:34 -0600

    Added ApplicationDbContext class

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+    public DbSet<Category> Categories { get; set; }
+  }
+}
