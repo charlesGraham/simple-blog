bb43ec38c13a85cd482d174c302ad74f4da5aee9
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-13 12:43:29 -0600

    Added DbContext into Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,13 +10,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("SimpleBlogConnectionString"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
