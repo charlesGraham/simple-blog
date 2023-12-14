94673a33ff766f34131e1f4eeb538b695414099e
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-14 16:04:27 -0600

    Added table to Categories page to show all available categories

--- client/SimpleBlog/src/app/features/category/category-list/category-list.component.ts
+++ client/SimpleBlog/src/app/features/category/category-list/category-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Category } from '../models/category.model';
+import { CategoryService } from './../services/category.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+
+  categories?: Category[];
+
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.categoryService.getAllCategories()
+      .subscribe({
+        next: (response) => {
+          this.categories = response;
+        }
+      });
+  }
 
 }
