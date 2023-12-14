12915e19cc95fed43cefbe11fcf3f2a9f32ee2f0
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-14 15:49:43 -0600

    Added getAllCategories to category-service + created Category model

--- client/SimpleBlog/src/app/features/category/services/category.service.ts
+++ client/SimpleBlog/src/app/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class CategoryService {
 
   addCategory(model: AddCategoryRequest): Observable<void> {
     return this.http.post<void>(`http://localhost:5146/api/Categories`, model);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`http://localhost:5146/api/Categories`);
   }
 }
