b61f79248ed376cf9206318a6498324f6f52b649
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-14 16:08:26 -0600

    Used async pipe so Observable code is a little bit more concise

--- client/SimpleBlog/src/app/features/category/category-list/category-list.component.ts
+++ client/SimpleBlog/src/app/features/category/category-list/category-list.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Category } from '../models/category.model';
 import { CategoryService } from './../services/category.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,17 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  categories?: Category[];
+  categories$?: Observable<Category[]>;
 
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
-    this.categoryService.getAllCategories()
-      .subscribe({
-        next: (response) => {
-          this.categories = response;
-        }
-      });
+    this.categories$ = this.categoryService.getAllCategories();
   }
 
 }
