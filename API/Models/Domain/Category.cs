f29ab97fb6a0e5cbc4efad4c968ca58c3ead8023
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-16 15:25:36 -0600

    Made relationship properties not nullable anymore. YOLO the warnings

--- API/Models/Domain/Category.cs
+++ API/Models/Domain/Category.cs
@@ -5,7 +5,7 @@ namespace API.Models.Domain
     public Guid Id { get; set; }
     public string? Name { get; set; } = string.Empty;
     public string? UrlHandle { get; set; } = string.Empty;
-    public ICollection<BlogPost>? BlogPosts { get; set; }
+    public ICollection<BlogPost> BlogPosts { get; set; }
 
   }
 }
