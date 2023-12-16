184ec23168842e6443780a2a6baa848e5cdcc75f
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-15 23:31:33 -0600

    Added getAllBlogPosts to blog-post.service + added table to display in blog-post-list.html

--- client/SimpleBlog/src/app/features/blog-post/blog-post-list/blog-post-list.component.ts
+++ client/SimpleBlog/src/app/features/blog-post/blog-post-list/blog-post-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { BlogPostService } from './../services/blog-post.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog-post-list',
   templateUrl: './blog-post-list.component.html',
   styleUrls: ['./blog-post-list.component.css']
 })
-export class BlogPostListComponent {
+export class BlogPostListComponent implements OnInit {
+
+  blogPosts$?: Observable<BlogPost[]>;
+
+  constructor(private blogPostService: BlogPostService) { }
+
+  ngOnInit(): void {
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+    console.log(this.blogPosts$);
+  }
 
 }
