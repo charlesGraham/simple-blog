9ecf2a5263bd2c1796f75c40f2bc7234eeebae99
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-17 10:20:49 -0600

    Added Category selector in add-blog-post.component

--- client/SimpleBlog/src/app/features/blog-post/add-blog-post/add-blog-post.component.ts
+++ client/SimpleBlog/src/app/features/blog-post/add-blog-post/add-blog-post.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
+import { CategoryService } from '../../category/services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-add-blog-post',
   templateUrl: './add-blog-post.component.html',
   styleUrls: ['./add-blog-post.component.css']
 })
-export class AddBlogPostComponent {
+export class AddBlogPostComponent implements OnInit {
 
   model: AddBlogPost;
+  categories$?: Observable<Category[]>;
 
-  constructor(private blogPostService: BlogPostService, private router: Router) {
+  constructor(private blogPostService: BlogPostService,
+    private router: Router, private categoryService: CategoryService) {
     this.model = {
       title: '',
       shortDescription: '',
@@ -24,6 +29,9 @@ export class AddBlogPostComponent {
       isVisible: true
     }
 
+  }
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
   }
 
   handleSubmit(): void {
