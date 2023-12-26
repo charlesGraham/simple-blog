e0385437e57001875ebae6c6a3d354d52529ffe8
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-25 20:27:03 -0600

    Added role-based authorization to GetAllCategories

--- API/Controllers/CategoriesController.cs
+++ API/Controllers/CategoriesController.cs
@@ -6,136 +6,133 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class CategoriesController : ControllerBase
-  {
-    private readonly ICategoryRepository _categoryRepository;
-
-    public CategoriesController(ICategoryRepository categoryRepository)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
     {
-      this._categoryRepository = categoryRepository;
-    }
+        private readonly ICategoryRepository _categoryRepository;
 
-    [HttpPost]
-    public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
-    {
-      // map DTO to domain model
-      var category = new Category
-      {
-        Name = request.Name,
-        UrlHandle = request.UrlHandle
-      };
-
-      await _categoryRepository.CreateAsync(category);
-
-      // domain model to DTO
-      var response = new CategoryDto
-      {
-        Id = category.Id,
-        Name = category.Name,
-        UrlHandle = category.UrlHandle
-      };
-
-      return Ok(response);
-    }
-
-    [HttpGet]
-    [Authorize]
-    public async Task<IActionResult> GetAllCategories()
-    {
-      // API call
-      var categories = await _categoryRepository.GetAllAsync();
-
-      // domain model to DTO
-      var response = new List<CategoryDto>();
-
-      foreach (var category in categories)
-      {
-        response.Add(new CategoryDto
+        public CategoriesController(ICategoryRepository categoryRepository)
         {
-          Id = category.Id,
-          Name = category.Name,
-          UrlHandle = category.UrlHandle
-        });
-      }
+            this._categoryRepository = categoryRepository;
+        }
 
-      return Ok(response);
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // map DTO to domain model
+            var category = new Category { Name = request.Name, UrlHandle = request.UrlHandle };
 
+            await _categoryRepository.CreateAsync(category);
+
+            // domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Writer")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            // API call
+            var categories = await _categoryRepository.GetAllAsync();
+
+            // domain model to DTO
+            var response = new List<CategoryDto>();
+
+            foreach (var category in categories)
+            {
+                response.Add(
+                    new CategoryDto
+                    {
+                        Id = category.Id,
+                        Name = category.Name,
+                        UrlHandle = category.UrlHandle
+                    }
+                );
+            }
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetCategoryById(Guid id)
+        {
+            var existingCategory = await _categoryRepository.GetById(id);
+
+            if (existingCategory is null)
+            {
+                return NotFound();
+            }
+
+            // domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = existingCategory.Id,
+                Name = existingCategory.Name,
+                UrlHandle = existingCategory.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateCategory(Guid id, UpdateCategoryRequestDto request)
+        {
+            // DTO to domain model
+            var category = new Category
+            {
+                Id = id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            category = await _categoryRepository.UpdateAsync(category);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            // domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            var category = await _categoryRepository.DeleteAsync(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            // domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
     }
-
-    [HttpGet]
-    [Route("{id:Guid}")]
-    public async Task<IActionResult> GetCategoryById(Guid id)
-    {
-      var existingCategory = await _categoryRepository.GetById(id);
-
-      if (existingCategory is null)
-      {
-        return NotFound();
-      }
-
-      // domain model to DTO
-      var response = new CategoryDto
-      {
-        Id = existingCategory.Id,
-        Name = existingCategory.Name,
-        UrlHandle = existingCategory.UrlHandle
-      };
-
-      return Ok(response);
-    }
-
-    [HttpPut]
-    [Route("{id:Guid}")]
-    public async Task<IActionResult> UpdateCategory(Guid id, UpdateCategoryRequestDto request)
-    {
-      // DTO to domain model
-      var category = new Category
-      {
-        Id = id,
-        Name = request.Name,
-        UrlHandle = request.UrlHandle
-      };
-
-      category = await _categoryRepository.UpdateAsync(category);
-
-      if (category == null)
-      {
-        return NotFound();
-      }
-
-      // domain model to DTO
-      var response = new CategoryDto
-      {
-        Id = category.Id,
-        Name = category.Name,
-        UrlHandle = category.UrlHandle
-      };
-
-      return Ok(response);
-    }
-
-    [HttpDelete]
-    [Route("{id:Guid}")]
-    public async Task<IActionResult> DeleteCategory(Guid id)
-    {
-      var category = await _categoryRepository.DeleteAsync(id);
-
-      if (category is null)
-      {
-        return NotFound();
-      }
-
-      // domain model to DTO
-      var response = new CategoryDto
-      {
-        Id = category.Id,
-        Name = category.Name,
-        UrlHandle = category.UrlHandle
-      };
-
-      return Ok(response);
-    }
-  }
 }
