1ccb05b164ceea6ad5599e644c97a74c8a53d3c7
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-25 22:40:40 -0600

    Added AuthGuards to block admin routes

--- client/SimpleBlog/src/app/app-routing.module.ts
+++ client/SimpleBlog/src/app/app-routing.module.ts
@@ -9,15 +9,40 @@ import { EditBlogPostComponent } from './features/blog-post/edit-blog-post/edit-
 import { HomeComponent } from './features/public/home/home.component';
 import { BlogDetailsComponent } from './features/public/blog-details/blog-details.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { authGuard } from './features/auth/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'admin/categories', component: CategoryListComponent },
-  { path: 'admin/categories/add', component: AddCategoryComponent },
-  { path: 'admin/categories/:id', component: EditCategoryComponent },
-  { path: 'admin/blogposts', component: BlogPostListComponent },
-  { path: 'admin/blogposts/add', component: AddBlogPostComponent },
-  { path: 'admin/blogposts/:id', component: EditBlogPostComponent },
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'admin/categories/add',
+    component: AddCategoryComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'admin/categories/:id',
+    component: EditCategoryComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'admin/blogposts',
+    component: BlogPostListComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'admin/blogposts/add',
+    component: AddBlogPostComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'admin/blogposts/:id',
+    component: EditBlogPostComponent,
+    canActivate: [authGuard]
+  },
   { path: 'blog/:url', component: BlogDetailsComponent },
   { path: 'login', component: LoginComponent }
 
